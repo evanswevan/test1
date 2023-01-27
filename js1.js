f557aaeb0bcecbf69d98abb4a9d4f04157873a4f
evanswevan <91506848+evanswevan@users.noreply.github.com>
2023-01-26 19:36:35 -0800

    Create js1.js

---
+++ js1.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  fs.readFile('home1.html', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+});
+
+const port = 3000;
+server.listen(port);
+console.log(`Server listening on port ${port}`);
